d1a198faea31967c695acdbf520ddc93d3986f9f
zurgur <harryfreyr@gmail.com>
2018-09-14 21:57:48 +0000

    location to users

--- login/forms.py
+++ login/forms.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from .models import Profile
 from django import forms
 
 class UserForm(forms.ModelForm):
@@ -8,3 +9,8 @@ class UserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username','password','email']
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['location']
--- login/views.py
+++ login/views.py
@@ -1,34 +1,39 @@
 from django.shortcuts import render, redirect, HttpResponse
 from django.contrib.auth import authenticate, login
 from django.views.generic import View
-from .forms import UserForm
+from .forms import UserForm, ProfileForm
 
 
 class userFormView(View):
-    form_class = UserForm
+    user_form = UserForm
+    profile_form = ProfileForm
+
     template_name = 'login/registration_form.html'
 
-    def get(self, reqest):
-        form =self.form_class(None)
-        return render(reqest, self.template_name, {'form': form})
+    def get(self, request):
+        userForm =self.user_form(None)
+        profileForm = self.profile_form(None)
+        return render(request, self.template_name, {'user_form': userForm, 'profile_form': profileForm})
 
     # prosses form data
-    def post(self, reqest):
-        form =self.form_class(reqest.POST)
+    def post(self, request):
+        userForm =self.user_form(request.POST, instance=request.user)
+        profileForm = self.profile_form(request.POST, instance=request.user.profile)
+        if userForm.is_valid() and profileForm.is_valid():
+            user = userForm.save(commit=False)
 
-        if form.is_valid():
-            user = form.save(commit=False)
-
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password']
+            username = userForm.cleaned_data['username']
+            password = userForm.cleaned_data['password']
             user.set_password(password)
             user.save()
+            profileForm.save()
 
             user = authenticate(username=username, password=password)
+
             if user is not None and user.is_active:
-                login (reqest, user)
-                return redirect('/api')
+                login (request, user)
+                return redirect('/')
                 
-        return render(reqest, self.template_name, {'form': form})
+        return render(request, self.template_name, {'user_form': userForm, 'profile_form': profileForm})
 
 
